f70c3bf618a4819c218dbf0618e40b487e3f7a66
deasadiqbal <asad95298@gmail.com>
2024-03-30 23:42:53 +0500

    project setup done

---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path 
+import logging
+
+project_name = "churn_prediction"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/constants/__init__.py",
+    "data/external_data",
+    "data/raw_data",
+    "data/transformed_data",
+    "config/config.yaml",
+    "webapp/static",
+    "webapp/templates",
+    "model",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/research.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the files: {filename}")
+    if (not os.path.exists(filepath)):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Created file: {filepath}")
+
+    else:
+        logging.info(f"File already exists: {filepath}")
